cec3c9eefa4ba8c35dd0efd76f4e5d58ca8e870c
Yamshchikova Evgeniya <31244230+Kinkandju@users.noreply.github.com>
2023-12-20 15:28:24 +0300

    feature: add constructor stack

--- src/stack.js
+++ src/stack.js
@@ -11,21 +11,42 @@ const { NotImplementedError } = require('../extensions/index.js');
  * stack.pop(); // returns the top element from stack and deletes it, returns 1
  * stack.pop(); // undefined
  *
- */
+*/
+
+//! Stack structure:
+//! LIFO - Last In First Out
+
+//? -----------------
+//?   1 2 3 4 ...  <-> 
+//? -----------------
+
+//! Base operation: PUSH / POP / TOP / SIZE / IS_EMPTY
+//* PUSH - adds an element to the top
+//* POP - takes away the top element
+//* TOP - a pointer that allows you to see what lies at the top (in this case, the element is not removed from the stack)
+//* SIZE - determines how many items are contained in the stack
+//* IS_EMPTY - a method for checking the stack for the presence or absence of elements in it
+
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this.storage = {},
+    this.size = 0
+  }
+
+  push(element) {
+    this.size += 1;
+    this.storage[this.size] = element;
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let removedElement = this.storage[this.size];
+    delete this.storage[this.size];
+    this.size -= 1;
+    return removedElement;
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.storage[this.size];
   }
 }
 
